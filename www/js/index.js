9b90fe950a3ddfc8a40b76a22f9b9d2dede1bc34
Sam Croft <samcroft@gmail.com>
2012-09-30 10:18:33 +0100

    Typo correction

--- www/js/index.js
+++ www/js/index.js
@@ -68,7 +68,7 @@ $(function(){
 		return false;
 	});
 	
-	//change to .tap to .click for browser testing
+	//change .tap to .click for browser testing
 	$('.button').tap(function(e){
 		var nextPage = $(e.target.hash);
 		var currentPage = $('.page.current');
